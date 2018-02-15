f65eb9cbc3bb35d0afc21de0cc98e3823779e4a6
Steven Tran <stran1@ocdsb.ca>
2018-02-14 20:25:10 -0500

    Fixes

--- src/Commands/Spine.cpp
+++ src/Commands/Spine.cpp
@@ -8,7 +8,6 @@ SpineUp::SpineUp() {
 
 // Called just before this Command runs the first time
 void SpineUp::Initialize() {
-	//double checkencoderspine = Robot::arm->CheckEncoders();
 	SmartDashboard::PutString("Spine", "Start");
 	Robot::arm->DifferentialSpine(0, 0);
 
@@ -17,24 +16,23 @@ void SpineUp::Initialize() {
 // Called repeatedly when this Command is scheduled to run
 void SpineUp::Execute() {
 	SmartDashboard::PutNumber("DPAD", Robot::oi->driveStick->GetPOV(0));
-		if (Robot::oi->driveStick->GetPOV(0) == 0){
-			double speed = (0.2);
-			SmartDashboard::PutString("Arm", "Moving");
-			Robot::arm->DifferentialSpine(speed, speed * Robot::arm->AdjustSpine());
-			SmartDashboard::PutNumber("Arm Encoder", Robot::arm->GetArmPosition());
-		}
-		else if(Robot::oi->driveStick->GetPOV(0) == -1){
-			double speed = (0);
-			SmartDashboard::PutString("Arm", "Moving");
-			Robot::arm->DifferentialSpine(speed, speed);
-			SmartDashboard::PutNumber("Arm Encoder", Robot::arm->GetArmPosition());
-		}
-		else if(Robot::oi->driveStick->GetPOV(0) == 180){
-			double speed = (-0.4);
-			SmartDashboard::PutString("Arm", "Moving");
-			Robot::arm->DifferentialSpine(speed, speed * Robot::arm->AdjustSpine());
-			SmartDashboard::PutNumber("Arm Encoder", Robot::arm->GetArmPosition());
-		}
+	SmartDashboard::PutNumber("Spine Encoder 1", Robot::arm->GetSpinePos1());
+	SmartDashboard::PutNumber("Spine Encoder 2", Robot::arm->GetSpinePos2());
+	if (Robot::oi->driveStick->GetPOV(0) == 0){
+		double speed = (0.2);
+		SmartDashboard::PutString("Arm", "Moving");
+		Robot::arm->DifferentialSpine(speed, speed * Robot::arm->AdjustSpine());
+	}
+	else if(Robot::oi->driveStick->GetPOV(0) == -1){
+		double speed = (0);
+		SmartDashboard::PutString("Arm", "Moving");
+		Robot::arm->DifferentialSpine(speed, speed);
+	}
+	else if(Robot::oi->driveStick->GetPOV(0) == 180){
+		double speed = (-0.2);
+		SmartDashboard::PutString("Arm", "Moving");
+		Robot::arm->DifferentialSpine(speed, speed * Robot::arm->AdjustSpine());
+	}
 
 }
 
